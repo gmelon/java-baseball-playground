dcf0a198694fad5c066b19e991bc791a7dfa7a7d
gmelon <hsh1769@naver.com>
2022-07-16 19:09:42 +0900

    feat: 입력 및 출력 로직

---
+++ src/main/java/baseball/InputView.java
@@ -0,0 +1,13 @@
+package baseball;
+
+import java.util.Scanner;
+
+public class InputView {
+
+    private static Scanner sc = new Scanner(System.in);
+
+    public static String getInput() {
+        return sc.nextLine();
+    }
+
+}
---
+++ src/main/java/baseball/ResultView.java
@@ -0,0 +1,9 @@
+package baseball;
+
+public class ResultView {
+
+    public static void showResult(String result) {
+        System.out.println(result);
+    }
+
+}
